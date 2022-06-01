0b31228702ef76b3c59f471b107733869593b9b0
Monster0506 <tjraklovits@gmail.com>
2022-06-01 13:18:07 -0400

    Prepared bootstrap, added autocd to bashrc

--- bash/.bashrc
+++ bash/.bashrc
@@ -117,22 +117,17 @@ if [ -f ~/.bash_functions.sh ]; then
 fi
 
 shopt -s cdspell
+shopt -s autocd
 #include dotfiles in wildcard expansion, and match case-insensitively
 shopt -s dotglob
 shopt -s nocaseglob
 # don't use the shell's built-in history mechanism
 
 
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
-fi
+
+
+
+
 if [ -f ~/.xprofile ]; then
   source ~/.xprofile
 fi
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,43 @@
-#! /usr/bin/bash
+#!/bin/bash
 # check if running as sudo
-if [ ! "$EUID" -ne 0 ]; then
+if [[ $EUID -eq 0 ]]; then
     echo "Please do not run as root"
     echo "If superuser is required, you will be prompted."
     exit
 fi
 testWget(){
-if [ ! command -v wget &> /dev/null ]; then
+if ! command -v wget &> /dev/null; then
     echo "wget is not installed. This will be installed later."
     WGET=false
+else
+    WGET=true
 fi
 }
-function installWgetRequired() {
+installWgetRequired() {
     wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf && mv Fira\ Code\ Regular\ Nerd\ Font\ Complete.ttf $HOME/.fonts/
+
+    # install neovim 
+    wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb
+    sudo apt install ./nvim-linux64.deb
+    
+    # 
+
 }
 
 
 
 #Make sure repo is up-to-date
-git pull origin master
+#git pull origin master
 
 # Check if bashrc exists.
+
+
+# dry run with no folders made
 if [ -f $HOME/.bashrc ]; then
     mv $HOME/.bashrc $HOME/.bashrc.back
 fi
 
-if [ -f $HOME/.bash_functions.sh]; then
+if [ -f $HOME/.bash_functions.sh ]; then
     mv $HOME/.bash_functions.sh $HOME/.bash_functions.sh.back
 fi
 
@@ -34,9 +46,8 @@ if [ ! -d $HOME/.config ]; then
 fi
 
 if [ ! -d $HOME/.config/nvim ]; then
-    mkdir $HOME/.config/nvim
+    mkdir $HOME/.config/nvim 
 fi
-
 if [ ! -d $HOME/.config/i3 ]; then
     mkdir $HOME/.config/i3
 fi
@@ -44,7 +55,6 @@ fi
 if [ ! -d $HOME/.fonts/ ]; then
     mdkir $HOME/.fonts
 fi
-
 
 # Create dotfiles directory, or if it exists, prompt user for install location
 if [ ! -d $HOME/.cfg/ ]; then
@@ -60,28 +70,41 @@ else
         INSTALLDIR=$HOME/.cfg/
     else
         echo "Where would you like to install?"
-        read INSTALLDIR
-        if [ ! -d $INSTALLDIR ]; then
+        read INSTALLDIR1
+        if [ ! -d $HOME/$INSTALLDIR1 ]; then
+            echo "making $INSTALLDIR1 in $HOME"
+            INSTALLDIR=$HOME/$INSTALLDIR1
             mkdir $INSTALLDIR
+        else
+            INSTALLDIR=$HOME/$INSTALLDIR1
         fi
     fi
-fi
+fi 
+curl -fsSL https://starship.rs/install.sh | bash
+# Install node through fnm
+curl -fsSL https://fnm.vercel.app/install | bash
+# Install vim-plug for neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo 
 
 
 
 # Install starship
-curl -fsSL https://starship.rs/install.sh | bash
-# Install node through fnm
-curl -fsSL https://fnm.vercel.app/install | bash
-
 
 # Install stuff that requires wget
-testWget()
+testWget
+if [ ! $WGET ]; then
+    installWgetRequired
+else
+    sudo apt install wget -y && installWgetRequired
 
+fi
+
+# install other useful stuff
+sudo apt install fzf python3 python3-pip vim vim-gtk3 pandoc lynx libnotify-bin i3 xcape -y
 
 
 
 
 # Move dotfiles to INSTALLDIR and syslink
+
