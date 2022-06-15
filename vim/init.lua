9ea50680e03a2a65065486b08213c7ec58fab36a
Monster0506 <tjraklovits@gmail.com>
2022-06-15 15:10:26 -0400

    Add more coc settings to init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -95,8 +95,6 @@ nnoremap <silent> K :call Show_documentation()<CR>
 
 inoremap <silent><expr> <TAB>pumvisible() ? "\<C-n>" :Check_back_space() ? "\<TAB>" :coc#refresh()
 " Make <CR> auto-select the first completion item and notify coc.nvim to
-" format on enter, <cr> could be remapped by other vim plugin
-inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm(): "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
 inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
 
 function! Check_back_space() abort
