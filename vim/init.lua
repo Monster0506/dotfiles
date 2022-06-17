7ba07dd561d588701dcd756a0e1e56865046e2ce
Monster0506 <tjraklovits@gmail.com>
2022-06-16 20:28:53 -0400

    Use nvim-cmp instead of coc

--- vim/init.lua
+++ vim/init.lua
@@ -1,87 +1,86 @@
 #!/usr/bin/env lua
-
 HOME = os.getenv("HOME")
-local Plug = vim.fn['plug#']
-vim.call('plug#begin')
-        Plug 'preservim/nerdtree' 
-        Plug 'mattn/webapi-vim'
-        Plug 'morhetz/gruvbox'
-        Plug 'easymotion/vim-easymotion' 
-        Plug 'sbdchd/neoformat' 
-        Plug 'ryanoasis/vim-devicons'
-        Plug 'preservim/nerdcommenter'
-        Plug 'tpope/vim-repeat'
-        Plug 'tpope/vim-surround'
-        Plug 'mg979/vim-visual-multi'
-        Plug 'mattn/emmet-vim'
-        Plug 'ap/vim-css-color'
-        Plug 'vim-airline/vim-airline'
-        Plug 'kien/ctrlp.vim'
-        Plug 'sheerun/vim-polyglot'
-        Plug 'vim-syntastic/syntastic'
-        Plug 'neovim/nvim-lspconfig'
-        Plug 'Honza/vim-snippets'
-        Plug 'rust-lang/rust.vim'
-        Plug 'github/copilot.vim'
-        Plug ('neoclide/coc.nvim', {branch='release'})
+local Plug = vim.fn["plug#"]
+vim.call("plug#begin")
+Plug "preservim/nerdtree"
+Plug "mattn/webapi-vim"
+Plug "morhetz/gruvbox"
+Plug "easymotion/vim-easymotion"
+Plug "sbdchd/neoformat"
+Plug "ryanoasis/vim-devicons"
+Plug "preservim/nerdcommenter"
+Plug "tpope/vim-repeat"
+Plug "tpope/vim-surround"
+Plug "mg979/vim-visual-multi"
+Plug "mattn/emmet-vim"
+Plug "ap/vim-css-color"
+Plug "vim-airline/vim-airline"
+Plug "kien/ctrlp.vim"
+Plug "sheerun/vim-polyglot"
+Plug "vim-syntastic/syntastic"
+Plug "hrsh7th/nvim-cmp"
+Plug "neovim/nvim-lspconfig"
+Plug "hrsh7th/cmp-nvim-lsp"
+Plug "hrsh7th/cmp-buffer"
+Plug "hrsh7th/cmp-path"
+Plug "hrsh7th/cmp-cmdline"
+Plug "hrsh7th/nvim-cmp"
+Plug "Honza/vim-snippets"
+-- For ultisnips users.
+Plug "SirVer/ultisnips"
+Plug "quangnguyen30192/cmp-nvim-ultisnips"
 
-vim.call('plug#end')
+Plug "nvim-treesitter/nvim-treesitter"
+Plug "rust-lang/rust.vim"
+Plug "github/copilot.vim"
+--Plug ('neoclide/coc.nvim', {branch='release'})
+
+vim.call("plug#end")
 --print(HOME)
 vim.opt.background = "dark"
-vim.opt.relativenumber=true
-vim.opt.number=true
-vim.opt.mouse="a"
-vim.opt.smartcase=true
-vim.opt.undodir="~/.vim/undodir"
-vim.opt.expandtab=true
-vim.opt.backspace="indent,eol,start"
-vim.opt.ignorecase=true
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.mouse = "a"
+vim.opt.smartcase = true
+vim.opt.undodir = "~/.vim/undodir"
+vim.opt.expandtab = true
+vim.opt.backspace = "indent,eol,start"
+vim.opt.ignorecase = true
 
-vim.opt.backup=false
-vim.opt.swapfile=false
-vim.opt.ignorecase=true
-vim.opt.wildignore="*.docx,*.pdf,*.exe,*.mcmeta,*.xlsx"
-vim.opt.colorcolumn="80"
-vim.opt.foldmethod="marker"
-vim.colorsheme="gruvbox"
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.ignorecase = true
+vim.opt.wildignore = "*.docx,*.pdf,*.exe,*.mcmeta,*.xlsx"
+vim.opt.colorcolumn = "80"
+vim.opt.foldmethod = "marker"
+vim.colorsheme = "gruvbox"
 vim.cmd([[colorscheme gruvbox]])
-vim.api.nvim_set_keymap(
-        "n",
-        "<C-t>",
-        ":NERDTreeToggle<cr>",
-        { noremap = true, silent=true }
-)
+vim.api.nvim_set_keymap("n", "<C-t>", ":NERDTreeToggle<cr>", {noremap = true, silent = true})
 vim.cmd([[
 
 
 ]])
-vim.api.nvim_set_keymap(
-        "n",
-        "<c-_>",
-        "<plug>NERDCommenterToggle",
-        { noremap = true }
-)
+vim.api.nvim_set_keymap("n", "<c-_>", "<plug>NERDCommenterToggle", {noremap = true})
 
-vim.api.nvim_set_keymap(
-        "n",
-        "<C-LeftMouse>",
-        "<LeftMouse>:call Show_documentation()<CR>",
-        { noremap = true, silent = true }
-	)
+--vim.api.nvim_set_keymap(
+--"n",
+--"<C-LeftMouse>",
+--"<LeftMouse>:call Show_documentation()<CR>",
+--{noremap = true, silent = true}
+--)
 
-vim.g.airline_right_alt_sep = ''
-vim.g.airline_right_sep = ''
-vim.g.airline_left_alt_sep= ''
-vim.g.airline_left_sep = ''
+vim.g.airline_right_alt_sep = ""
+vim.g.airline_right_sep = ""
+vim.g.airline_left_alt_sep = ""
+vim.g.airline_left_sep = ""
 
-
-
- --vim.NERDTreeWinPos="right"
+--vim.NERDTreeWinPos="right"
 --require("nvim-lsp-installer").setup {}
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
 -- Keybindings I am too lazy to put in the proper format. {{{
-vim.cmd([[
+vim.cmd(
+    [[
 
 nnoremap <silent> <C-t> :NERDTreeToggle<CR>
 let NERDTreeWinPos = "right"  
@@ -89,48 +88,6 @@ augroup fmt
   autocmd!
   autocmd BufWritePre * silent Neoformat
 augroup END
-nnoremap gd <Plug>(coc-definition)
-nnoremap <F2> <Plug>(coc-rename)
-nnoremap <silent> K :call Show_documentation()<CR>
-
-inoremap <silent><expr> <TAB>pumvisible() ? "\<C-n>" :Check_back_space() ? "\<TAB>" :coc#refresh()
-" Make <CR> auto-select the first completion item and notify coc.nvim to
-inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-
-function! Check_back_space() abort
-  let col = col('.') - 1
-  return !col || getline('.')[col - 1]  =~# '\s'
-endfunction
-function! Show_documentation()
-  if (index(['vim','help'], &filetype) >= 0)
-    execute 'h '.expand('<cword>')
-  elseif (coc#rpc#ready())
-    call CocActionAsync('doHover')
-  else
-    execute '!' . &keywordprg . " " . expand('<cword>')
-  endif
-endfunction
-if has('nvim')
-  inoremap <silent><expr> <c-space> coc#refresh()
-else
-  inoremap <silent><expr> <c-@> coc#refresh()
-endif
-" Use `[g` and `]g` to navigate diagnostics
-" Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
-nmap <silent> [g <Plug>(coc-diagnostic-prev)
-nmap <silent> ]g <Plug>(coc-diagnostic-next)
-nmap <silent> gy <Plug>(coc-type-definition)
-nmap <silent> gi <Plug>(coc-implementation)
-nmap <silent> gr <Plug>(coc-references)
-" Example: `<leader>aap` for current paragraph
-xmap <leader>a  <Plug>(coc-codeaction-selected)
-nmap <leader>a  <Plug>(coc-codeaction-selected)
-
-" Remap keys for applying codeAction to the current buffer.
-nmap <leader>ac  <Plug>(coc-codeaction)
-" Apply AutoFix to problem on the current line.
-nmap <leader>qf  <P
-autocmd CursorHold * silent call CocActionAsync('highlight')
 noremap (<CR> (<CR>)<Esc>O
 inoremap (;    (<CR>);<Esc>O 
 inoremap (,    (<CR>),<Esc>O
@@ -183,24 +140,96 @@ function! NextTextObject(motion)
   let c = nr2char(getchar())
   exe "normal! f".c."v".a:motion.c
 endfunction
-  if has('nvim-0.4.0') || has('patch-8.2.0750')
-  nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-  nnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
-  inoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
-  inoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
-  vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-  vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
-endif
-        ]])
--- }}}
--- }}}
 
-vim.api.nvim_set_keymap(
-  "i",
-  "<CR>",
-  "<Plug>(coc-snippets-expand)",
-  {
-    noremap = true,
-    silent = true,
-  }
+        ]]
 )
+-- }}}
+-- }}}
+local cmp = require "cmp"
+
+cmp.setup(
+    {
+        snippet = {
+            -- REQUIRED - you must specify a snippet engine
+            expand = function(args)
+                vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            end
+        },
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered()
+        },
+        mapping = cmp.mapping.preset.insert(
+            {
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({select = true}) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }
+        ),
+        sources = cmp.config.sources(
+            {
+                {name = "nvim_lsp"},
+                {name = "ultisnips"},
+                {name = "buffer"}
+            }
+        )
+    }
+)
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype(
+    "gitcommit",
+    {
+        sources = cmp.config.sources(
+            {
+                {name = "cmp_git"} -- You can specify the `cmp_git` source if you were installed it.
+            },
+            {
+                {name = "buffer"}
+            }
+        )
+    }
+)
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(
+    "/",
+    {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            {name = "buffer"}
+        }
+    }
+)
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(
+    ":",
+    {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources(
+            {
+                {name = "path"}
+            },
+            {
+                {name = "cmdline"}
+            }
+        )
+    }
+)
+
+-- Setup lspconfig.
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require "lspconfig".rust_analyzer.setup {
+    capabilities = capabilities
+}
+require "lspconfig".pyright.setup {
+    capabilities = capabilities
+}
+
+require "lspconfig".tsserver.setup {
+    capabilities = capabilities
+}
