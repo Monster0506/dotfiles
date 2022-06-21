f6049d8b3f171555b108a6ffaf42909c43e3be96
monster0506 <tjraklovits@gmail.com>
2022-06-21 14:01:27 -0400

    add nightly config to rustup installation in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -149,6 +149,7 @@ installExtraStuff() {
 
 configureRust(){
     rustup component add rustfmt
+    rustup default nightly
     rustup component add rust-src
     curl -L https://github.com/rust-anayzer/rst-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
     chmod +x ~/.local/bin/rust-analyzer
