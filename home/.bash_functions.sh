e6139af486a553b63e9de921d33cd8e7e499261e
monster0506 <tjraklovits@gmail.com>
2022-06-29 23:18:00 -0400

    Update server function with python3 code

--- home/.bash_functions.sh
+++ home/.bash_functions.sh
@@ -481,14 +481,6 @@ if [ $? -eq 0 ]; then
     }
 fi
 
-server() {
-    local port="${1:-8000}"
-    sleep 1 && open "http://localhost:${port}/" &
-    # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
-    # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
-    python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
-}
-
 # Create a data URL from a file
 dataurl() {
     local mimeType=$(file -b --mime-type "$1")
@@ -565,5 +557,13 @@ how() {
             echo $filename | grep "$1"
         done
     done
+
+}
+
+server() {
+    local port=${1:-8080}
+    echo $port
+    xdg-open "http://localhost:$port" &
+    python3 -m http.server $port
 
 }
