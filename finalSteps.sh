9f9f141fe90815f204b2aa8b55966e92c037e0ae
monster0506 <tjraklovits@gmail.com>
2022-07-06 14:05:43 -0400

    Add support for root nvim

--- finalSteps.sh
+++ finalSteps.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/bash 
+#!/usr/bin/bash
 sudo ln -s $HOME/$INSTALLDIR/home/.profile $HOME/.profile
 mkdir $HOME/Cpp/
 cd $HOME/Cpp
 gh repo clone Utils
-cd ..
+cd ~
 gh repo clone Rust
-
 gh repo clone timelog
+
+sudo mkdir /root/.config/nvim /root/.local/share/nvim -p
+sudo cp ~/.config/nvim/* -r /root/.config/nvim
+sudo cp ~/.local/share/nvim/* -r /root/.local/share/nvim
