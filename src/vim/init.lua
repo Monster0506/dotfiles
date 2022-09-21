11469018d13eec2e0f1fe185a9b2999a37add733
monster0506 <91501317+Monster0506@users.noreply.github.com>
2022-09-21 17:47:40 -0400

    M feat: Update LSP keybindings with which-key -- src/vim/init.lua

--- src/vim/init.lua
+++ src/vim/init.lua
@@ -1,6 +1,5 @@
-#!/usr/bin/env lua
+---@diagostic disable: undefined-global
 HOME = os.getenv("HOME")
--- print(HOME)
 
 -- Global Vim Variables (vim.g) {{{
 local vimg = {
@@ -28,6 +27,7 @@ require("evil").setup() -- lua/evil.lua
 require("commands").setup() -- lua/commands.lua
 require("setup").run() -- lua/setup.lua
 require("nvim_cmp").setup() -- lua/nvim_cmp.lua
+local wk = require("which-key")
 --- }}}
 
 -- Misc Vim Settings {{{
@@ -100,42 +100,61 @@ local on_attach = function(client, bufnr)
     -- Mappings. See `:help vim.lsp.*` for documentation on any of the below
     -- functions
     local bufopts = {noremap = true, silent = true, buffer = bufnr}
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "K", ShowDocumentation, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set(
-        "n",
-        "<leader>wl",
-        function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end,
-        bufopts
+    wk.register(
+        {
+            d = {vim.lsp.buf.definition, "Go to definition"},
+            D = {vim.lsp.buf.declaration, "Go to declaration"},
+            r = {vim.lsp.buf.references, "Go to references"},
+            i = {vim.lsp.buf.implementation, "Go to implementation"}
+        },
+        {prefix = "g", buffer = bufnr}
     )
-    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-    vim.keymap.set("n", "<leader>ac", vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-    vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
+
+    wk.register(
+        {
+            w = {
+                name = "workspace",
+                a = {vim.lsp.buf.add_workspace_folder, "Add Workspace Folder"},
+                r = {vim.lsp.buf.remove_workspace_folder, "Remove Workspace Folder"},
+                l = {
+                    function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end,
+                    "View Workspace Folders"
+                }
+            },
+            D = {vim.lsp.buf.type_definition, "Type Definition"},
+            rn = {vim.lsp.buf.rename, "Rename"},
+            ac = {vim.lsp.buf.code_action, "Code Action"}
+        },
+        {prefix = "<leader>"}
+    )
+
+    wk.register(
+        {
+            K = {ShowDocumentation, "Show Documentation"},
+            ["<C-k>"] = {vim.lsp.buf.signature_help, "Signature Help"},
+            ["<Space>"] = {vim.lsp.buf.format, "Format"}
+        }
+    )
 end
+
 function ShowDocumentation()
     local filetype = vim.bo.filetype
     local winid = require("ufo").peekFoldedLinesUnderCursor()
     if not winid then
-        if vim.tbl_contains({"vim", "help"}, filetype) then
+        if vim.fn.expand("%:t") == "Cargo.toml" then
+            require("crates").show_popup()
+        elseif vim.tbl_contains({"vim", "help"}, filetype) then
             vim.cmd("h " .. vim.fn.expand("<cword>"))
         elseif vim.tbl_contains({"man"}, filetype) then
             vim.cmd("Man " .. vim.fn.expand("<cword>"))
-        elseif vim.fn.expand("%:t") == "Cargo.toml" then
-            require("crates").show_popup()
         else
             vim.lsp.buf.hover()
         end
     end
 end
+
 --- }}}
 -- Servers {{{
 local servers = {
@@ -187,3 +206,4 @@ require("mason-installer").setup(
     }
 )
 --- }}}
+--
