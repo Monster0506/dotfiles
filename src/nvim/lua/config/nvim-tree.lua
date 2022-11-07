bac3cce05a773828ac53ccd09fc29dba0dce3eb6
monster0506 <>
2022-11-06 19:11:29 -0500

    M feat: Update nvim-tree setup -- src/nvim/lua/config/nvim-tree.lua

--- src/nvim/lua/config/nvim-tree.lua
+++ src/nvim/lua/config/nvim-tree.lua
@@ -1,8 +1,14 @@
 require("nvim-tree").setup(
     {
         -- {{{
+        disable_netrw = true,
+        open_on_setup = true,
+        open_on_setup_file = true,
+        sync_root_with_cwd = true,
+
         view = {
             -- {{{
+            centralize_selection = true,
             side = "right",
             mappings = {
                 -- {{{
@@ -16,12 +22,22 @@ require("nvim-tree").setup(
             }
             -- }}}
         },
+        renderer = {
+            add_trailing = true,
+            highlight_git = true,
+            highlight_opened_files = "all",
+
+        },
         filters = {
             -- {{{
             dotfiles = false,
             custom = {"^.git$"}
             -- }}}
-        }
+        },
+        diagnostics = {
+            enable = true,
+
+        },
         -- }}}
     }
 )
