947099b6c708e2065ad865ce1ada2fb62fbfab9d
monster0506 <>
2022-11-22 22:45:26 -0500

    M feat: Update listchars -- src/nvim/lua/options/options.lua

--- src/nvim/lua/options/options.lua
+++ src/nvim/lua/options/options.lua
@@ -1,35 +1,42 @@
-    -- Options (vim.opt) {{{
-    local vimopts = {
-        -- {{{
-        background = "dark",
-        termguicolors = true,
-        foldcolumn = "0",
-        foldlevel = 99,
-        splitright = true,
-        splitbelow = true,
-        foldlevelstart = 99,
-        foldenable = true,
-        relativenumber = true,
-        number = true,
-        undofile = true,
-        smartcase = true,
-        mouse = "a",
-        guifont = "FiraCode Nerd Font:h15",
-        ignorecase = true,
-        expandtab = true,
-        backup = false,
-        swapfile = false,
-        wildignore = "*.docx,*.pdf,*.exe,*.mcmeta,*.xlsx",
-        colorcolumn = "80",
-        foldmethod = "syntax",
-        concealcursor = "nc",
-        list = true
-        -- }}}
+-- Options (vim.opt) {{{
+local vimopts = {
+    -- {{{
+    background = "dark",
+    termguicolors = true,
+    foldcolumn = "0",
+    foldlevel = 99,
+    splitright = true,
+    splitbelow = true,
+    foldlevelstart = 99,
+    foldenable = true,
+    relativenumber = true,
+    number = true,
+    undofile = true,
+    smartcase = true,
+    mouse = "a",
+    guifont = "FiraCode Nerd Font:h15",
+    ignorecase = true,
+    expandtab = true,
+    backup = false,
+    swapfile = false,
+    wildignore = "*.docx,*.pdf,*.exe,*.mcmeta,*.xlsx",
+    colorcolumn = "80",
+    foldmethod = "syntax",
+    concealcursor = "nc",
+    list = true,
+    listchars = {
+        ["tab"] = "» ",
+        ["trail"] = "·",
+        ["extends"] = "<",
+        ["precedes"] = ">",
+        ["conceal"] = "┊",
+        ["nbsp"] = "␣",
+        ["eol"] = "↴"
     }
-    for k, v in pairs(vimopts) do -- {{{
-        vim.opt[k] = v
-        -- }}}
-    end
-    vim.opt.listchars:append("eol:↴")
-    --- }}}}}}
-
+    -- }}}
+}
+for k, v in pairs(vimopts) do -- {{{
+    vim.opt[k] = v
+    -- }}}
+end
+--- }}}}}}
