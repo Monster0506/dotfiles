0dbf96d6e977aa7cc135df2298738b3e7d6a36b3
monster0506 <12682102-monster0506@users.noreply.gitlab.com>
2023-02-19 18:31:00 -0500

    M feat: Add toggable keybinds -- src/nvim/lua/keybinds/toggle.lua

---
+++ src/nvim/lua/keybinds/toggle.lua
@@ -0,0 +1,43 @@
+local wk = require("which-key")
+
+wk.register(
+    {
+        v = {
+            ':<C-U>set <C-R>=(&virtualedit =~# "all") ? "virtualedit-=all" : "virtualedit+=all"<CR> virtualedit?<CR>',
+            "Toggle 'virtualedit'"
+        },
+        s = {"<Cmd>setlocal spell! spelllang=en_us | setlocal spell? <CR> ", "Toggle Spell"},
+        l = {"<Cmd>setlocal list! list? <CR>", "Toggle 'list'"},
+        b = {'<Cmd>lua vim.o.bg = vim.o.bg == "dark" and "light" or "dark"; print(vim.o.bg)<CR>', "Toggle 'background'"},
+        n = {
+            name = "Number",
+            n = {"<Cmd>setlocal number! number? <CR>", "Toggle 'number'"},
+            r = {"<Cmd>setlocal relativenumber! relativenumber? <CR>", "Toggle 'relativenumber"}
+        },
+        c = {
+            name = "Cursor",
+            c = {
+                "<Cmd>setlocal cursorcolumn! cursorcolumn?<CR>",
+                "Toggle 'cursorcolumn'"
+            },
+            l = {
+                "<Cmd>setlocal cursorline! cursorline?<CR>",
+                "Toggle 'cursorline'"
+            }
+        },
+        r = {"<Cmd>setlocal ruler! ruler?<CR>", "Toggle 'ruler'"},
+        d = {
+            name = "Diff",
+            t = {"<cmd>diffthis<cr>", "Diff On"},
+            o = {"<cmd>diffoff<cr>", "Diff Off"}
+        }
+    },
+    {
+        prefix = "<leader>t",
+        name = "toggle/tag"
+    }
+)
+
+vim.cmd [[
+ set <C-R>=(&virtualedit =~# "all") ? "virtualedit-=all" : "virtualedit+=all"<CR>
+]]
