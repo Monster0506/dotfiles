b9f0db75dc192b8e8795b2281887436f42fcafb8
monster0506 <12682102-monster0506@users.noreply.gitlab.com>
2023-02-19 22:40:06 -0500

    M feat: Add source file keymap -- src/nvim/lua/keybinds/keybindings.lua

--- src/nvim/lua/keybinds/keybindings.lua
+++ src/nvim/lua/keybinds/keybindings.lua
@@ -5,10 +5,10 @@ local keymap = vim.api.nvim_set_keymap
 
 -- Keybindings {{{
 -- Miscellaneous Mappings {{{
--- keymap("n", "<F1>", "<cmd>setlocal spell! spelllang=en_us<CR>", opts)
+-- keymap("n", "<F1>", "<Cmd>setlocal spell! spelllang=en_us<CR>", opts)
 keymap("c", "cd.", "lcd %:p:h<CR>", opts)
 keymap("c", "cwd", "lcd %:p:h<CR>", opts)
-keymap("i", "<leader><C-i>", "<cmd>PickEverythingInsert<CR>", opts)
+keymap("i", "<leader><C-i>", "<Cmd>PickEverythingInsert<CR>", opts)
 keymap("x", "<", "<gv", opts)
 keymap("x", ">", ">gv", opts)
 wk.register {
@@ -16,7 +16,7 @@ wk.register {
 }
 
 wk.register {
-    ["<C-t>"] = {"<cmd>NvimTreeToggle<CR>", "File Tree"}
+    ["<C-t>"] = {"<Cmd>NvimTreeToggle<CR>", "File Tree"}
 }
 wk.register(
     {
@@ -28,17 +28,18 @@ wk.register(
 
 wk.register(
     {
+        s = {':<C-R>=(&filetype =~# "lua" ? "luafile %" : "source %")<CR><CR>', "Source file"},
         l = {
             ":<c-u><c-r><c-r>='let @'. v:register .' = '. string(getreg(v:register))<cr><c-f><left>",
             "Modify Registers"
         },
-        ["<C-i>"] = {"<cmd>PickEverythingInsert<CR>", "Pick Everything"},
+        ["<C-i>"] = {"<Cmd>PickEverythingInsert<CR>", "Pick Everything"},
         t = {
-            t = {"<cmd>Vista nvim_lsp<CR>", "LSP Markers"},
+            t = {"<Cmd>Vista nvim_lsp<CR>", "LSP Markers"},
             a = {
                 name = "Vista",
-                g = {"<cmd>Vista ctags<CR>", "Ctags"},
-                m = {"<cmd>Vista nvim_lsp<CR>", "LSP Markers"}
+                g = {"<Cmd>Vista ctags<CR>", "Ctags"},
+                m = {"<Cmd>Vista nvim_lsp<CR>", "LSP Markers"}
             }
         },
         y = {'"+y', "Yank to Clipboard"},
