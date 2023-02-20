bb62949e534c354833d371d2abc3cc2a2f276651
monster0506 <12682102-monster0506@users.noreply.gitlab.com>
2023-02-19 22:51:14 -0500

    M feat: Add more Git commands -- src/nvim/lua/keybinds/git.lua

--- src/nvim/lua/keybinds/git.lua
+++ src/nvim/lua/keybinds/git.lua
@@ -2,24 +2,24 @@ local keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 local wk = require("which-key")
 -- Git Keybindings {{{
-keymap("n", "<leader>g", "<CMD>G<CR>", opts)
+keymap("n", "<leader>g", "<Cmd>G<CR>", opts)
 wk.register(
     {
-        g = {
-            name = "git",
-            h = {
-                name = "hunk",
-                s = {"<cmd>Gitsigns stage_hunk<CR>", "stage"},
-                u = {"<cmd>Gitsigns undo_stage_hunk<CR>", "unstage"}
-            },
-            d = {"<CMD>Gdiffsplit<CR>", "View Diff"},
-            ["]"] = {"<CMD>Gitsigns prev_hunk<CR>", "Previous Hunk"},
-            ["["] = {"<CMD>Gitsigns next_hunk<CR>", "Next Hunk"},
-            c = {"<CMD>G commit<CR>", "Commit"},
-            a = {"<CMD>G add %<CR>", "Add"},
-            r = {"<CMD>G reset %<CR>", "Reset"}
-        }
+        name = "git",
+        h = {
+            name = "hunk",
+            s = {"<Cmd>Gitsigns stage_hunk<CR>", "stage"},
+            u = {"<Cmd>Gitsigns undo_stage_hunk<CR>", "unstage"}
+        },
+        d = {"<Cmd>Gdiffsplit<CR>", "View Diff"},
+        ["]"] = {"<Cmd>Gitsigns prev_hunk<CR>", "Previous Hunk"},
+        ["["] = {"<Cmd>Gitsigns next_hunk<CR>", "Next Hunk"},
+        c = {"<Cmd>G commit<CR>", "Commit"},
+        a = {"<Cmd>Gwrite<CR>", "Add"},
+        w = {"<Cmd>Gwrite<CR>", "Add"},
+        r = {"<Cmd>G reset %<CR>", "Reset"},
+        l = {"<Cmd>Gclog<CR>", "Log"}
     },
-    {prefix = "<leader>"}
+    {prefix = "<leader>g"}
 )
 --- }}}
