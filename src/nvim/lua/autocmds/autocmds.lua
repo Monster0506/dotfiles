f5a016dddc07d125cc2a3af27d73f4ac105686ed
monster0506 <12682102-monster0506@users.noreply.gitlab.com>
2023-03-26 18:53:05 +0000

    M feat: Remove auto open nvim-tree -- src/nvim/lua/autocmds/autocmds.lua

--- src/nvim/lua/autocmds/autocmds.lua
+++ src/nvim/lua/autocmds/autocmds.lua
@@ -19,11 +19,4 @@ vim.api.nvim_create_autocmd(
     }
 )
 --- }}}
--- Open NvimTree on startup {{{
-local function open_nvim_tree()
-    -- open the tree
-    require("nvim-tree.api").tree.open()
-end
-vim.api.nvim_create_autocmd({"VimEnter"}, {callback = open_nvim_tree})
--- }}}
 --- }}}
