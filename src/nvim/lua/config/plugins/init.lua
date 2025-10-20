03685463191b2b6e3c247205a5b249762cd675f0
Monster0506 <tjraklovits@gmail.com>
2025-10-20 00:48:17 -0400

    Add universial runner plugin

--- src/nvim/lua/config/plugins/init.lua
+++ src/nvim/lua/config/plugins/init.lua
@@ -3,4 +3,21 @@ require("config.plugins.telescope")
 require("config.plugins.miniclue")
 require("config.plugins.codecompanion")
 require("config.plugins.tjlang").setup()
-require("config.plugins.asm").setup()
+require("config.plugins.runner").setup({
+	runners = {
+		asm = {
+			run = "java -jar C:\\Users\\TJ\\.vscode\\extensions\\ahmz1833.mars-mips-1.0.4\\mars.jar nc me %file%",
+			filetypes = { "asm", "mars", "mips" },
+			run_command = "MarsRun",
+		},
+		python = {
+			run = "python %file%",
+			filetypes = { "python" },
+			run_command = "PyRun",
+		},
+		lua = {
+			run = "lua %file%",
+			filetypes = { "lua" },
+		},
+	},
+})
